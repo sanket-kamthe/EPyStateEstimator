551c9860190ef15c3daed2eab31f59c1f14f64af
sanket-kamthe <s.kamthe15@imperial.ac.uk>
2018-10-18 15:46:32 +0100

    saving before switch to multipledispatch

---
+++ MomentMatching/Mapper.py
@@ -0,0 +1,41 @@
+# Copyright 2018 Sanket Kamthe
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class MappingTransform:
+
+    def __init__(self, approximation_method=None, **kwargs):
+        self.params = {}  # Dictionary containing parameters for the moment matching approximation
+
+        self.method = approximation_method
+        self.params.update(kwargs)
+        # print(self.params)
+        for key in self.params:
+            self.__setattr__(key, self.params[key])
+
+    def _transform(self, func, state):
+        """
+        Returns the gaussian approximation of the integral
+
+        \int f(x) q (x) dx, where q(x) is the distribution and f(x) is the non-linear function
+        The result is exact when f(x) is a linear function.
+        :param nonlinear_func:
+        :param distribution: object of type GaussianState for example
+        :return: distribution of y =  \int f(x) q (x) dx in the form of a tuple
+        mean (y), variance (yyT), cross_covariance(xyT)
+        """
+        return NotImplementedError
+
+    def __call__(self, func, state):
+        return self._transform(func=func, state=state)
