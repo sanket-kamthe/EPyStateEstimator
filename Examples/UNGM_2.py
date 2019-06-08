EP and power EP working. Fixed wierd scaling problem

Examples/UNGM_2.py
@@ -82,16 +82,16 @@ def power_sweep(trans_id='UT', power=1, damping=1, dim=1, samples=int(1e5)):
 SEED = 100
 
 np.random.seed(seed=SEED)
-sys_dim = 1
+sys_dim = 4
 N = 100
 system = UniformNonlinearGrowthModel()
-# system = BearingsOnlyTracking()
+system = BearingsOnlyTracking()
 # system = BearingsOnlyTrackingTurn()
 data = system.simulate(N)
 x_true, x_noisy, y_true, y_noisy = zip(*data)
 
 
-con = sqlite3.connect("temp.db", detect_types=sqlite3.PARSE_DECLTYPES)
+con = sqlite3.connect("temp_bear.db", detect_types=sqlite3.PARSE_DECLTYPES)
 db = con.cursor()
 table_name = 'UNGM_SIM'
 create_dynamics_table(db, name=table_name)

MomentMatching/Estimator.py
@@ -43,18 +43,19 @@ class Estimator:
     def proj_trans(self, func, state):
         xx_mean, xx_cov, _ = self.trans_map(func, state)
         xx_cov += self.transition_noise
-        xx_cov /= self.power
+        # xx_cov /= self.power
         # np.linalg.cholesky(xx_cov)
         pred_state = Gaussian(xx_mean, xx_cov)
         return pred_state
 
     def proj_meas(self, func, state, meas):
+        meas = np.atleast_1d(meas)
         np.linalg.cholesky(state.cov)
         z_mean, z_cov, xz_cross_cov = \
             self.meas_map(func, state)
 
         z_cov += self.measurement_noise
-        z_cov /= self.power
+        # z_cov /= self.power
         np.linalg.cholesky(z_cov)
         # kalman_gain = np.matmul(xz_cross_cov, np.linalg.pinv(z_cov))
         kalman_gain = np.linalg.solve(z_cov, xz_cross_cov.T).T
@@ -69,7 +70,7 @@ class Estimator:
             self.trans_map(func, state)
 
         xx_cov += self.transition_noise
-        xx_cov /= self.power
+        # xx_cov /= self.power
 
         # J = xx_cross_cov @ np.linalg.pinv(xx_cov)
         J = np.linalg.solve(xx_cov, xx_cross_cov.T).T

StateModel/Gaussian.py
@@ -100,13 +100,13 @@ class Gaussian:
         #         self.dim = dim
 
         if mean_vec is not None:
-            self.mean = mean_vec
+            self.mean = np.atleast_1d(mean_vec)
             self.cov = cov_mat
             self._mode = 'moment'
 
         if shift_vec is not None:
             self.precision = precision_mat
-            self.shift = shift_vec
+            self.shift = np.atleast_1d(shift_vec)
             self._mode = 'natural'
 
         # super(multivariate_normal, self).__init__(mean=mean_vec,

Utils/linalg.py
@@ -17,6 +17,7 @@ import numpy as np
 
 JIT = 1e-6
 
+
 def jittered_chol(a, jitter=None, lower=True, overwrite_a=False, check_finite=True):
     if jitter is None:
         jitter = JIT
@@ -38,11 +39,10 @@ def jittered_solve(a, b, jitter=None, overwrite_a=False, overwrite_b=False, assu
     x = solve(a, b, overwrite_a=overwrite_a, overwrite_b=overwrite_b, assume_a=assume_a, transposed=transposed)
     return x
 
+
 def validate_covariance(state):
 
     np.linalg.cholesky(state.cov)
-
-
 
 
 if __name__=="__main__":